c0a004ba1c54d57502520d3c6fb2ab4b82e4998b
dtompkins <mail+github@dtompkins.com>
2009-10-11 11:55:11 -0700

    Fix bug in LaunchBrowser

--- fbcmd.php
+++ fbcmd.php
@@ -2821,6 +2821,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
   function LaunchBrowser($url) {
+    global $fbcmdPrefs;
     global $hasLaunched;
     $hasLaunched = true;
     if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
