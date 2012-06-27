d1ac3b4ecfa210f8107cf65dcb605c2ab0086ac9
dtompkins <mail+github@dtompkins.com>
2012-06-26 23:39:32 -0400

    updated WIX path for packaging

--- package/msi/buildmsi.bat
+++ package/msi/buildmsi.bat
@@ -1,4 +1,4 @@
-set path=%path%;C:\Program Files\Windows Installer XML v3\bin
+set path=%path%;C:\Program Files (x86)\Windows Installer XML v3.5\bin
 del Install-fbcmd.msi
 candle fbcmd.wxs 
 light -ext WixUIExtension -cultures:en-US fbcmd.wixobj -out Install-fbcmd.msi
